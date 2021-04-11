7dafe72a51bbb155377eadcb27b31b87604df17e
wind1050 <33870844+wind1050@users.noreply.github.com>
2021-04-11 23:58:27 +0200

    trying to draw the earth

---
+++ Assets/mover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour
+{
+    public LocationInfo pos;
+    public float breitengrad;
+    public float längengrad;
+
+    public GameObject großeKugel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = großeKugel.transform.position + new Vector3(Mathf.Sin((längengrad/180)*Mathf.PI)*Mathf.Cos((breitengrad/180)*Mathf.PI)
+                                                                             ,Mathf.Sin((breitengrad/180)*Mathf.PI)
+                                                                             ,-Mathf.Cos((längengrad/180)*Mathf.PI)*Mathf.Cos((breitengrad/180)*Mathf.PI))
+            * großeKugel.transform.localScale.x/2;
+         
+    }
+}
